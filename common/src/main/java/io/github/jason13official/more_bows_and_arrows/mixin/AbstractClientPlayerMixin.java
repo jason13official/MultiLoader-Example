0b4d35cc87e0f7bdbae85cd237ab0021f49a59fd
jason13official <jason13official@gmail.com>
2026-05-13 21:39:33 -0400

    Update AbstractClientPlayerMixin.java

--- common/src/main/java/io/github/jason13official/more_bows_and_arrows/mixin/AbstractClientPlayerMixin.java
+++ common/src/main/java/io/github/jason13official/more_bows_and_arrows/mixin/AbstractClientPlayerMixin.java
@@ -1,5 +1,6 @@
 package io.github.jason13official.more_bows_and_arrows.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.authlib.GameProfile;
 import io.github.jason13official.more_bows_and_arrows.item.ModBowItem;
 import net.minecraft.client.Minecraft;
@@ -8,12 +9,15 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(AbstractClientPlayer.class)
 public abstract class AbstractClientPlayerMixin extends Player {
@@ -22,21 +26,8 @@ public abstract class AbstractClientPlayerMixin extends Player {
     super(level, pos, yRot, gameProfile);
   }
 
-  @Inject(at = @At("HEAD"), method = "getFieldOfViewModifier", cancellable = true)
-  private void more_bows_and_arrows$getFieldOfViewModifier(CallbackInfoReturnable<Float> cir) {
-
-    float modifier = 1.0F;
-
-    if (this.getAbilities().flying) {
-      modifier *= 1.1F;
-    }
-
-    modifier *= ((float)this.getAttributeValue(Attributes.MOVEMENT_SPEED) / this.getAbilities().getWalkingSpeed() + 1.0F) / 2.0F;
-    if (this.getAbilities().getWalkingSpeed() == 0.0F || Float.isNaN(modifier) || Float.isInfinite(modifier)) {
-      modifier = 1.0F;
-    }
-
-    ItemStack itemstack = this.getUseItem();
+  @Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/player/AbstractClientPlayer;getUseItem()Lnet/minecraft/world/item/ItemStack;", shift = Shift.AFTER), method = "getFieldOfViewModifier", cancellable = true)
+  private void more_bows_and_arrows$getFieldOfViewModifier(CallbackInfoReturnable<Float> cir, @Local float modifier, @Local ItemStack itemstack) {
 
     if (itemstack.getItem() instanceof ModBowItem) {
       int useTicks = this.getTicksUsingItem();
